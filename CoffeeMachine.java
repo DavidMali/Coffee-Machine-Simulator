edaf03e0d15535b76a0c31e427cc839a3b28edcc
DavidMali <davidmmalinowski@gmail.com>
2024-06-08 18:47:53 +0200

    Refactoring

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -13,31 +13,7 @@ public class CoffeeMachine {
     private static final Beverage espresso = new Beverage(250, 0, 16, 4);
     private static final Beverage latte = new Beverage(350, 75, 20, 7);
     private static final Beverage cappuccino = new Beverage(200, 100, 12, 6);
-    /*
-    private static final int waterPerCup = 200;
-    private static final int milkPerCup = 50;
-    private static final int coffeBeansPerCup = 15;
-     */
 
-    public static void main(String[] args) {
-        getUserChoiceLoop();
-        /*int waterAmount = promptForWaterAmount();
-        int milkAmount = promptForMilkAmount();
-        int coffeBeansAmount = promptForCoffeeBeansAmount();
-        int possibleCoffeeCups = calculatePossibleCoffeeCups(waterAmount, milkAmount, coffeBeansAmount);
-        int numberOfCups = promptForNumberOfCups();
-        printPossibleCoffeCups(numberOfCups, possibleCoffeeCups);
-
-         printRequiredIngredients(promptForNumberOfCups());
-         System.out.println("Starting to make a coffee\n" +
-                "Grinding coffee beans\n" +
-                "Boiling water\n" +
-                "Mixing boiled water with crushed coffee beans\n" +
-                "Pouring coffee into the cup\n" +
-                "Pouring some milk into the cup\n" +
-                "Coffee is ready!"); */
-        scanner.close();
-    }
 
     public static void printMachineContents() {
         System.out.printf("The coffee machine has:\n" +
@@ -48,26 +24,20 @@ public class CoffeeMachine {
                 "$%d of money\n\n", amountOfWater, amountOfMilk, amountOfCoffee, amountOfCups, amountOfMoney);
     }
 
-    public static void getUserChoiceLoop() {
-        while (true) {
-            System.out.println("Write action (buy, fill, take, remaining, exit):");
-            String choice = scanner.nextLine();
-            switch (choice.toLowerCase()) {
-                case "buy":
-                    buyBeverage();
-                    break;
-                case "fill":
-                    fillMachine();
-                    break;
-                case "take":
-                    takeMoney();
-                    break;
-                case "remaining":
-                    printMachineContents();
-                    break;
-                case "exit":
-                    return;
-            }
+    public static void processUserChoice(String choice) {
+        switch (choice.toLowerCase()) {
+            case "buy":
+                buyBeverage();
+                break;
+            case "fill":
+                fillMachine();
+                break;
+            case "take":
+                takeMoney();
+                break;
+            case "remaining":
+                printMachineContents();
+                break;
         }
 
     }
@@ -78,20 +48,25 @@ public class CoffeeMachine {
         if (userInput.equalsIgnoreCase("back")) {
             return;
         }
-        int choice = Integer.parseInt(userInput);
-        switch (choice) {
-            case 1:
-                makeBeverage(espresso);
-                break;
-            case 2:
-                makeBeverage(latte);
-                break;
-            case 3:
-                makeBeverage(cappuccino);
-                break;
-            default:
-                buyBeverage();
+        try {
+            int choice = Integer.parseInt(userInput);
+            switch (choice) {
+                case 1:
+                    makeBeverage(espresso);
+                    break;
+                case 2:
+                    makeBeverage(latte);
+                    break;
+                case 3:
+                    makeBeverage(cappuccino);
+                    break;
+                default:
+                    buyBeverage();
+            }
+        }  catch (Exception e) {
+            buyBeverage();
         }
+
     }
 
     public static void fillMachine() {
@@ -138,52 +113,4 @@ public class CoffeeMachine {
         return true;
     }
 
-
-    /*
-    public static int promptForNumberOfCups() {
-        System.out.println("Write how many cups of coffee you will need:");
-        return scanner.nextInt();
-    }
-
-    public static void printRequiredIngredients(int numberOfCups) {
-        System.out.printf("For %d cups of coffee you will need:\n", numberOfCups);
-        System.out.println(numberOfCups * waterPerCup + " ml of water");
-        System.out.println(numberOfCups * milkPerCup + " ml of milk");
-        System.out.println(numberOfCups * coffeBeansPerCup + " g of coffee beans");
-    }
-
-    public static int promptForWaterAmount() {
-        System.out.println("Write how many ml of water the coffee machine has:");
-        return scanner.nextInt();
-    }
-
-    public static int promptForMilkAmount() {
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        return scanner.nextInt();
-    }
-
-    public static int promptForCoffeeBeansAmount() {
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        return scanner.nextInt();
-    }
-
-    public static int calculatePossibleCoffeeCups(int waterAmount, int milkAmount, int coffeeBeansAmount) {
-        int possibleWaterCups = waterAmount / waterPerCup;
-        int possibleMilkCups = milkAmount / milkPerCup;
-        int possibleCoffeeBeansCups = coffeeBeansAmount / coffeBeansPerCup;
-        int highestCommonDenominator;
-        highestCommonDenominator = Math.min(possibleWaterCups, Math.min(possibleMilkCups, possibleCoffeeBeansCups));
-        return highestCommonDenominator;
-    }
-
-    public static void printPossibleCoffeCups(int numberOfCups, int possibleCoffeeCups) {
-        if (numberOfCups > possibleCoffeeCups) {
-            System.out.println("No, I can make only " + possibleCoffeeCups +" cup(s) of coffee");
-        } else if (numberOfCups == possibleCoffeeCups) {
-            System.out.println("Yes, I can make that amount of coffee ");
-        } else {
-            System.out.println("Yes, I can make that amount of coffee (and even "
-                    + (possibleCoffeeCups - numberOfCups) + " more than that)");
-        }
-    } */
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
 package machine;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Write action (buy, fill, take, remaining, exit):");
+            String choice = scanner.nextLine();
+            if (choice.equals("exit")) {
+                scanner.close();
+                return;
+            }
+            CoffeeMachine.processUserChoice(choice);
+            }
+        }
 }
+
+
+
